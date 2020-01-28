e2757eb8379a36d19aa18013158ae3cdf260040f
Henry <hlai4395@gmail.com>
2020-01-28 10:13:21 +0000

    Input validation for user input code and procedure

---
+++ AgileCmd/TestInputCodeValidation/UnitTest1.cs
@@ -0,0 +1,43 @@
+using NUnit.Framework;
+using TestInputValidation;
+
+namespace TestInputValidation
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Class1 test1 = new Class1();
+            test1.displayuserMsg();
+
+            //string correctInput = "123";
+
+            //string twoDigitInput = "22";
+
+            //string noInput = " ";
+
+            string charInput = "123$$$%%%";
+
+
+            //var correctTest = test1.validateCode(correctInput);
+            //Assert.IsTrue(correctTest);
+
+            //var twoDigitTest = test1.validateCode(twoDigitInput);
+            //Assert.IsFalse(twoDigitTest);
+
+            //var noInputTest = test1.validateCode(noInput);
+            //Assert.IsFalse(noInputTest);
+
+            var charInputTest = test1.CleanUserInput(charInput);
+            Assert.IsTrue(charInputTest);
+
+        }
+    }
+}
