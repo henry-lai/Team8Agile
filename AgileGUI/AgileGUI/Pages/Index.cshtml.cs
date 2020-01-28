6c75f4d9752b4448059e283acda673e1be527f71
Anthony Sweeney <2399626@dundee.ac.uk>
2020-01-28 10:37:00 +0000

    basic search and table displayed

--- AgileGUI/AgileGUI/Pages/Index.cshtml.cs
+++ AgileGUI/AgileGUI/Pages/Index.cshtml.cs
@@ -12,14 +12,35 @@ namespace AgileGUI.Pages
     {
         private readonly ILogger<IndexModel> _logger;
 
+        [BindProperty(SupportsGet = true)]
+        public string SearchString { get; set; }
+        // Requires using Microsoft.AspNetCore.Mvc.Rendering;
+
+        public static string UserInput = "";
+        
+        static Dictionary<string, string> temp = new Dictionary<string, string>() { { "Label", "best" },
+                { "Description", "desc" },
+                { "Name", "hello" },
+                { "Address", "the street" },
+                { "Zip", "12345" },
+                { "Cost", "12345678" },
+                { "Distance", "123456" },
+                { "Score", "123" }
+            };
+        public static List<Dictionary<string, string>> Data = new List<Dictionary<string, string>>() { temp };
+
+
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
+            
         }
 
         public void OnGet()
         {
-
+            
+            UserInput = SearchString;
+            
         }
     }
 }
